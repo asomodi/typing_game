0de03eac8ce455f28567a29b74f300eec476f628
Akos Somodi <akossomodi@yahoo.com>
2020-03-24 14:40:36 +0100

    add timer and ending screen

--- script.js
+++ script.js
@@ -41,6 +41,13 @@ let score = 0;
 // Init time
 let time = 10;
 
+// Focus (the cursor) on text on start
+text.focus();
+
+// Start counting down
+const timeInterval = setInterval(updateTime, 1000);
+
+// Generate random word from array
 function getRandomWord() {
   return words[Math.floor(Math.random() * words.length)];
 }
@@ -59,6 +66,28 @@ function updateScore() {
   scoreEl.innerHTML = score;
 }
 
+function updateTime() {
+  time--;
+  timeEl.innerHTML = time + 's';
+
+  if (time === 0) {
+    clearInterval(timeInterval);
+    // end game
+    gameOver();
+  }
+}
+
+// Game Over, show end screen
+function gameOver() {
+  endgameEl.innerHTML = `
+    <h1>Time ran out</h1>
+    <p>Your final score is ${score}</p>
+    <button onclick="location.reload()">Reload</button>
+    `;
+
+  endgameEl.style.display = 'flex';
+}
+
 addWordToDOM();
 
 // Event listeners
@@ -73,5 +102,9 @@ text.addEventListener('input', e => {
 
     // Clear
     e.target.value = '';
+
+    time += 5;
+
+    updateTime();
   }
 });
